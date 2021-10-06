065ac7e619135a2ed0e9da7b70cca83a022e4b70
Muhammad Idrees <midrees097@gmail.com>
2021-10-06 16:30:16 +0500

    added jsonData method to convert file to jsonData

--- lib/src/quote_repository.dart
+++ lib/src/quote_repository.dart
@@ -1,3 +1,6 @@
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:quoter/src/consts.dart';
 
 import 'models/models.dart';
@@ -16,5 +19,12 @@ class QuoteLocalRepository extends QuoteRepository {
   @override
   List<Quote> getQuotes() {
     return [];
+  }
+
+  Future<List<Map<String, dynamic>>> get jsonData async {
+    String _fileData = await File(filePath).readAsString();
+    return (json.decode(_fileData) as List)
+        .map((e) => e as Map<String, dynamic>)
+        .toList();
   }
 }
--- test/quote_local_repository_test.dart
+++ test/quote_local_repository_test.dart
@@ -4,11 +4,20 @@ import 'package:quoter/src/quote_repository.dart';
 import 'package:quoter/src/models/models.dart';
 
 void main() {
-  QuoteLocalRepository _quoteLocalRepository = QuoteLocalRepository();
+  String _testFilePath = "test/resources/quote_data.json";
+  QuoteLocalRepository _quoteLocalRepository =
+      QuoteLocalRepository(filePath: _testFilePath);
 
   group("QuoteLocalRepository", () {
     test("user 'kquoteJsonFilePath' when not specified", () {
       expect(QuoteLocalRepository().filePath, kquoteJsonFilePath);
+    });
+
+    test("converts json file to map list", () async {
+      List<Map<String, dynamic>> quoteMapList =
+          await _quoteLocalRepository.jsonData;
+
+      expect(quoteMapList, isA<List<Map<String, dynamic>>>());
     });
 
     test("gets a list of Quotes on getQuotes", () {
