9588f1957606de100572690c0b9df56891880ce5
Muhammad Idrees <midrees097@gmail.com>
2021-10-06 14:41:53 +0500

    added test for local file path

--- test/quote_local_repository_test.dart
+++ test/quote_local_repository_test.dart
@@ -1,13 +1,20 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:quoter/src/consts.dart';
 import 'package:quoter/src/quote_repository.dart';
 import 'package:quoter/src/models/models.dart';
 
 void main() {
   QuoteLocalRepository _quoteLocalRepository = QuoteLocalRepository();
 
-  test("gets a list of Quotes on getQuotes", () {
-    List<Quote> quoteList = _quoteLocalRepository.getQuotes();
+  group("QuoteLocalRepository", () {
+    test("user 'kquoteJsonFilePath' when not specified", () {
+      expect(QuoteLocalRepository().filePath, kquoteJsonFilePath);
+    });
 
-    expect(quoteList, isA<List<Quote>>());
+    test("gets a list of Quotes on getQuotes", () {
+      List<Quote> quoteList = _quoteLocalRepository.getQuotes();
+
+      expect(quoteList, isA<List<Quote>>());
+    });
   });
 }
