b8c8ce518350c32ac172760cce82ac8a4073d0c0
Muhammad Idrees <midrees097@gmail.com>
2021-10-06 13:25:30 +0500

    fixed import issue for entities

--- lib/src/quote_repository.dart
+++ lib/src/quote_repository.dart
@@ -3,3 +3,10 @@ import 'models/models.dart';
 abstract class QuoteRepository {
   List<Quote> getQuotes();
 }
+
+class QuoteLocalRepository extends QuoteRepository {
+  @override
+  List<Quote> getQuotes() {
+    throw UnimplementedError();
+  }
+}
