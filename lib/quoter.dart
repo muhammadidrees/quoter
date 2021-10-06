b9a570e54e89655624110b3307ef01b8ac645af1
Muhammad Idrees <midrees097@gmail.com>
2021-10-06 17:50:31 +0500

    created class quoter

--- lib/quoter.dart
+++ lib/quoter.dart
@@ -1,7 +1,4 @@
 library quoter;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'src/quoter.dart';
+export 'src/models/models.dart';
