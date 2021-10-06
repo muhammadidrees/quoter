37700e08504706f28c8899e9452043197003de55
Muhammad Idrees <midrees097@gmail.com>
2021-10-06 17:01:35 +0500

    implemeted getQuotes funtion and passed test :)

--- lib/src/quote_repository.dart
+++ lib/src/quote_repository.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 import 'dart:io';
 
 import 'package:quoter/src/consts.dart';
+import 'package:quoter/src/enitities/entities.dart';
 
 import 'models/models.dart';
 
@@ -18,11 +19,16 @@ class QuoteLocalRepository extends QuoteRepository {
 
   @override
   Future<List<Quote>> getQuotes() async {
-    return [];
+    List<Map<String, dynamic>> _jsonData = await jsonData;
+
+    return _jsonData
+        .map((json) => Quote.fromLocalEntitiy(QuoteLocalEntity.fromJson(json)))
+        .toList();
   }
 
   Future<List<Map<String, dynamic>>> get jsonData async {
     String _fileData = await File(filePath).readAsString();
+
     return (json.decode(_fileData) as List)
         .map((e) => e as Map<String, dynamic>)
         .toList();
