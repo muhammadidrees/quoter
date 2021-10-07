048cfd1adbcd338b141104c66a3e98fe6e761566
Muhammad Idrees <midrees097@gmail.com>
2021-10-07 15:21:19 +0500

    refactored add to create const values for testing

--- test/quote_local_repository_test.dart
+++ test/quote_local_repository_test.dart
@@ -3,18 +3,10 @@ import 'package:quoter/src/consts.dart';
 import 'package:quoter/src/repositories/repositories.dart';
 import 'package:quoter/src/models/models.dart';
 
+import 'resources/test_const.dart';
+
 void main() {
   String _testFilePath = "test/resources/quote_data.json";
-
-  List<Map<String, dynamic>> _testFileData = [
-    {"quotation": "I am the Boss", "quotee": "Muhammad Idrees"},
-    {"quotation": "My name is Hamza", "quotee": "Muhammad Hamza"}
-  ];
-
-  List<Quote> _testQuoteData = const [
-    Quote(quotation: "I am the Boss", quotee: "Muhammad Idrees"),
-    Quote(quotation: "My name is Hamza", quotee: "Muhammad Hamza"),
-  ];
 
   QuoteLocalRepository _quoteLocalRepository =
       QuoteLocalRepository(filePath: _testFilePath);
@@ -28,13 +20,13 @@ void main() {
       List<Map<String, dynamic>> quoteMapList =
           await _quoteLocalRepository.jsonData;
 
-      expect(quoteMapList, _testFileData);
+      expect(quoteMapList, ktestFileData);
     });
 
     test("gets a list of Quotes on getQuotes", () async {
       List<Quote> quoteList = await _quoteLocalRepository.getQuotes();
 
-      expect(quoteList, _testQuoteData);
+      expect(quoteList, ktestQuoteData);
     });
   });
 }
---
+++ test/resources/test_const.dart
@@ -0,0 +1,11 @@
+import 'package:quoter/src/models/models.dart';
+
+const List<Map<String, dynamic>> ktestFileData = [
+  {"quotation": "I am the Boss", "quotee": "Muhammad Idrees"},
+  {"quotation": "My name is Hamza", "quotee": "Muhammad Hamza"}
+];
+
+const List<Quote> ktestQuoteData = [
+  Quote(quotation: "I am the Boss", quotee: "Muhammad Idrees"),
+  Quote(quotation: "My name is Hamza", quotee: "Muhammad Hamza"),
+];
