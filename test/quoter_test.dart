d27ee29ee98766617899750229e6dc08cdc2fe9d
Muhammad Idrees <midrees097@gmail.com>
2021-10-07 15:13:32 +0500

    created mock of quote local repo for testing

--- test/quoter_test.dart
+++ test/quoter_test.dart
@@ -2,7 +2,12 @@ import 'package:flutter_test/flutter_test.dart';
 
 import 'package:quoter/quoter.dart';
 import 'package:quoter/src/repositories/repositories.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
 
+import 'quoter_test.mocks.dart';
+
+@GenerateMocks([QuoteLocalRepository])
 void main() {
   Quoter quoter = const Quoter();
 
