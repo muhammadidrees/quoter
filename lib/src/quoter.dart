15b11d36291afddb527037b4ff90b37c72a5aeb1
Muhammad Idrees <midrees097@gmail.com>
2021-10-07 15:42:18 +0500

    Basic implementation of getAllQuotes added
    CONTAINS MOCK ERROR

--- lib/src/quoter.dart
+++ lib/src/quoter.dart
@@ -1,7 +1,10 @@
 import 'repositories/repositories.dart';
+import 'models/models.dart';
 
 class Quoter {
   final QuoteRepository quoteRepository;
 
   const Quoter({this.quoteRepository = const QuoteLocalRepository()});
+
+  Future<List<Quote>> getAllQuotes() async => [];
 }
--- test/quoter_test.dart
+++ test/quoter_test.dart
@@ -6,6 +6,7 @@ import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
 
 import 'quoter_test.mocks.dart';
+import 'resources/test_const.dart';
 
 @GenerateMocks([QuoteLocalRepository])
 void main() {
@@ -14,6 +15,14 @@ void main() {
   group("Quoter", () {
     test("uses 'QuoteLocalRepository' when not specified", () {
       expect(const Quoter().quoteRepository, isA<QuoteLocalRepository>());
+    });
+
+    test("returns a list of quotes on getAllQuotes()", () async {
+      when(quoter.getAllQuotes()).thenAnswer((_) async => ktestQuoteData);
+
+      List<Quote> _quotes = await quoter.getAllQuotes();
+
+      expect(_quotes, []);
     });
   });
 }
