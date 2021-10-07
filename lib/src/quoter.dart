7d54a18668c20633e6f0b1ec1cb26b071c054a19
Muhammad Idrees <midrees097@gmail.com>
2021-10-07 13:24:59 +0500

    injected quote repository in quoter class

--- lib/src/quoter.dart
+++ lib/src/quoter.dart
@@ -1,1 +1,7 @@
-class Quoter {}
+import 'package:quoter/src/repositories/repositories.dart';
+
+class Quoter {
+  final QuoteRepository quoteRepository;
+
+  const Quoter({this.quoteRepository = const QuoteLocalRepository()});
+}
--- test/quoter_test.dart
+++ test/quoter_test.dart
@@ -1,7 +1,14 @@
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:quoter/quoter.dart';
+import 'package:quoter/src/repositories/repositories.dart';
 
 void main() {
   Quoter quoter = Quoter();
+
+  group("Quoter", () {
+    test("Uses 'QuoteLocalRepository' when not specified", () {
+      expect(const Quoter().quoteRepository, isA<QuoteLocalRepository>());
+    });
+  });
 }
