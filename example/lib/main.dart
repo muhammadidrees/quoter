02795fa7f0e1c90ed508943a6126ed937643274f
Muhammad Idrees <midrees097@gmail.com>
2021-10-12 12:34:23 +0500

    updated app and appbar titles

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -10,11 +10,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Quoter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Quoter Demo'),
     );
   }
 }
