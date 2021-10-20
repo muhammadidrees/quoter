fc3d556e6a372a3a4bc63586690e22a5c1a5cedc
Muhammad Idrees <midrees097@gmail.com>
2021-10-20 11:59:06 +0500

    Added quotes

--- lib/data/quote_data.dart
+++ lib/data/quote_data.dart
@@ -1,3 +1,45 @@
 part of 'data.dart';
 
-const List<Map<String, dynamic>> kquoteData = [];
+const List<Map<String, dynamic>> kquoteData = [
+  {
+    "quotation":
+        "I do not believe in taking the right decision, I take a decision and make it right.",
+    "quotee": "Muhammad Ali Jinnah"
+  },
+  {
+    "quotation":
+        "Two things are infinite: the universe and human stupidity; and I'm not sure about the universe.",
+    "quotee": "Albert Einstein"
+  },
+  {
+    "quotation": "Do what you can, with what you have, where you are.",
+    "quotee": "Theodore Roosevelt"
+  },
+  {
+    "quotation":
+        "Think a hundred times before you take a decision, but once that decision is taken, stand by it as one man.",
+    "quotee": "Muhammad Ali Jinnah"
+  },
+  {
+    "quotation": "Be yourself; everyone else is already taken.",
+    "quotee": "Oscar Wilde"
+  },
+  {
+    "quotation":
+        "If you can't explain it to a six year old, you don't understand it yourself.",
+    "quotee": "Albert Einstein"
+  },
+  {
+    "quotation":
+        "If you cannot do great things, do small things in a great way.",
+    "quotee": "Napoleon Hill"
+  },
+  {
+    "quotation": "Expect the best, prepare for the worst.",
+    "quotee": "Muhammad Ali Jinnah"
+  },
+  {
+    "quotation": "That which does not kill us makes us stronger.",
+    "quotee": "Friedrich Nietzsche"
+  },
+];
